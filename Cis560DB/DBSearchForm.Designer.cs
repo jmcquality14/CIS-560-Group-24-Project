055f9c3ec62ee506f0d0979cca1776ebec5605ff
jmcquality14 <you@example.com>
2019-04-22 19:40:24 -0500

    Progress on UI 4/22

--- Cis560DB/DBSearchForm.Designer.cs
+++ Cis560DB/DBSearchForm.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.uxSearchButton = new System.Windows.Forms.Button();
+            this.uxRatingBox = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.uxKeywordLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // uxSearchButton
@@ -40,20 +48,175 @@
             this.uxSearchButton.Text = "Search";
             this.uxSearchButton.UseVisualStyleBackColor = true;
             // 
+            // uxRatingBox
+            // 
+            this.uxRatingBox.FormattingEnabled = true;
+            this.uxRatingBox.Items.AddRange(new object[] {
+            "4 Stars and Above",
+            "3 Stars and Above",
+            "2 Stars and Above ",
+            "1 Star and Above"});
+            this.uxRatingBox.Location = new System.Drawing.Point(341, 76);
+            this.uxRatingBox.Name = "uxRatingBox";
+            this.uxRatingBox.Size = new System.Drawing.Size(121, 21);
+            this.uxRatingBox.TabIndex = 1;
+            this.uxRatingBox.Text = "--Select Rating--";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(401, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // uxKeywordLabel
+            // 
+            this.uxKeywordLabel.AutoSize = true;
+            this.uxKeywordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxKeywordLabel.Location = new System.Drawing.Point(12, 9);
+            this.uxKeywordLabel.Name = "uxKeywordLabel";
+            this.uxKeywordLabel.Size = new System.Drawing.Size(81, 20);
+            this.uxKeywordLabel.TabIndex = 3;
+            this.uxKeywordLabel.Text = "Keywords:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Director:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(275, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rating:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Year Released:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2019",
+            "2018",
+            "2017",
+            "2016",
+            "2015",
+            "2014",
+            "2013",
+            "2012",
+            "2011",
+            "2010",
+            "2009",
+            "2008",
+            "2007",
+            "2006",
+            "2005",
+            "2004",
+            "2003",
+            "2002",
+            "2001",
+            "2000",
+            "1999",
+            "1998",
+            "1997",
+            "1996",
+            "1995",
+            "1994",
+            "1993",
+            "1992",
+            "1991",
+            "1990",
+            "1989",
+            "1988",
+            "1987",
+            "1986",
+            "1985",
+            "1984",
+            "1983",
+            "1982",
+            "1981",
+            "1980",
+            "1979",
+            "1978",
+            "1977",
+            "1976",
+            "1975",
+            "1974",
+            "1973",
+            "1972",
+            "1971",
+            "1970",
+            "1969",
+            "1968",
+            "1967",
+            "1966",
+            "1965",
+            "1964",
+            "1963",
+            "1962",
+            "1961",
+            "1960"});
+            this.comboBox1.Location = new System.Drawing.Point(137, 75);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "--Select Year--";
+            // 
             // uxDBSearchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(512, 261);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uxKeywordLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.uxRatingBox);
             this.Controls.Add(this.uxSearchButton);
             this.Name = "uxDBSearchForm";
             this.Text = "DBSearchForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button uxSearchButton;
+        private System.Windows.Forms.ComboBox uxRatingBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label uxKeywordLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
